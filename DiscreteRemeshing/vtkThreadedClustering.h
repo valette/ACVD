156e671d39d5e716148ed2478cb13d69331b8532
Sebastien Valette <sebastien.valette@creatis.insa-lyon.fr>
2014-02-25 10:53:03 +0100

    fix vtkSimpleCriticalSection objects construction

--- DiscreteRemeshing/vtkThreadedClustering.h
+++ DiscreteRemeshing/vtkThreadedClustering.h
@@ -1060,8 +1060,8 @@ template < class Metric >
 	this->DisplayThreadsTimingsFlag = 0;
 
 	this->Timer = vtkTimerLog::New ();
-	this->PoolAllocationLock=vtkSimpleCriticalSection::New();
-	this->PoolAllocationLock2=vtkSimpleCriticalSection::New();	
+	this->PoolAllocationLock = new vtkSimpleCriticalSection;
+	this->PoolAllocationLock2 = new vtkSimpleCriticalSection;
 	this->EdgesProcess = 0;
 
 #ifdef THREADSAFECLUSTERING
@@ -1088,8 +1088,8 @@ template < class Metric >
 	vtkThreadedClustering < Metric >::~vtkThreadedClustering ()
 {
 	this->Timer->Delete ();
-	this->PoolAllocationLock->Delete();
-	this->PoolAllocationLock2->Delete();
+	delete this->PoolAllocationLock;
+	delete this->PoolAllocationLock2;
 	this->PoolQueue1->Delete();
 	this->PoolQueue2->Delete();
 
