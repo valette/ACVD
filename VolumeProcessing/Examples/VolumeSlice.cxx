d01dfdcb56d3d52659c9cf8f5bc0b852cf67ec32
Sebastien Valette <valette@localhost.localdomain>
2018-12-02 23:04:06 +0100

    VolumeSlice : fixes, read .nii.gz  files

--- VolumeProcessing/Examples/VolumeSlice.cxx
+++ VolumeProcessing/Examples/VolumeSlice.cxx
@@ -11,19 +11,33 @@ Auteur:   Sebastien Valette
 // .SECTION Description
 
 #include <sstream>
+#include <cstring>
 
-#include <vtkPNGWriter.h>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+#include <iostream>
+
 #include <vtkJPEGWriter.h>
 #include <vtkImageData.h>
-#include <vtkMetaImageReader.h>
+#include <vtkImageResample.h>
 #include <vtkImageReslice.h>
 #include <vtkImageShiftScale.h>
+#include <vtkImageShiftScale.h>
+#include <vtkLookupTable.h>
+#include <vtkMultiThreader.h>
+#include <vtkPNGWriter.h>
+#include <vtkTIFFWriter.h>
+#include <vtkTimerLog.h>
 #include <vtkXMLDataElement.h>
 #include <vtkXMLUtilities.h>
-#include <vtkMultiThreader.h>
-#include <vtkTimerLog.h>
 
-class MyThreaderHelperClass {
+#include "vtkRobustImageReader.h"
+
+using std::cout ;
+
+class MyThreaderHelperClass
+{
 public:
     vtkImageData *Image;
 
@@ -40,6 +54,10 @@ public:
 	double	nativeTransformMtrx[3][3];
 	char anatomicalOrientation[1000];
 
+	std::vector<vtkImageData*> Images;
+	std::vector<vtkImageReslice*> Slicers;
+	std::vector<vtkImageShiftScale*> Casters;
+
 	MyThreaderHelperClass() {
 		strcpy (prefix,"slice");
 		strcpy (directory, "");
@@ -51,21 +69,27 @@ public:
 		xyzOrientation = 0;
 	}
 
-	~MyThreaderHelperClass() {}
+	~MyThreaderHelperClass() {
+	}
 };
 
 VTK_THREAD_RETURN_TYPE ThreadedImageSlice (void *arg) {
 	vtkMultiThreader::ThreadInfo *Info = (vtkMultiThreader::ThreadInfo*) arg;
 	MyThreaderHelperClass *Helper = (MyThreaderHelperClass *) Info->UserData;
 	int MyId = Info->ThreadID;
-	int NumberOfThreads=Info->NumberOfThreads;
+	int NumberOfThreads = Info->NumberOfThreads;
 	
-	vtkImageData *Image=vtkImageData::New();
-	Image->ShallowCopy(Helper->Image);
-	vtkImageReslice *Slicer=vtkImageReslice::New();
+	vtkImageData *Image = Helper->Images[MyId];
+
+	vtkImageReslice *Slicer = Helper->Slicers[MyId];
+	Slicer->SetInputData(Image);
 	Slicer->SetNumberOfThreads(1);
-	vtkImageShiftScale *Cast=vtkImageShiftScale::New();
+	Slicer->SetOutputDimensionality(2);
+	Slicer->SetInterpolationModeToNearestNeighbor();
+
+	vtkImageShiftScale *Cast = Helper->Casters[MyId];
 	Cast->SetNumberOfThreads(1);
+
 	vtkImageWriter *Writer;
 	std::stringstream Suffix;
 
@@ -84,9 +108,9 @@ VTK_THREAD_RETURN_TYPE ThreadedImageSlice (void *arg) {
 			Dimensions[0] = orientationDims[2];
 			Dimensions[1] = orientationDims[1];
 			Dimensions[2] = 1;
-			params[0][0] = 0;	params[0][1] = 0;	params[0][2] = 1;
-			params[1][0] = 0;	params[1][1] = -1;	params[1][2] = 0;
-			params[2][0] = 1;	params[2][1] = 0;	params[2][2] = 0;
+			params[0][0]=0;		params[0][1]=0;		params[0][2]=1;
+			params[1][0]=0;		params[1][1]=-1;	params[1][2]=0;
+			params[2][0]=1;		params[2][1]=0;		params[2][2]=0;
 			Point[2] = 0;
 			Point[1] = 0;
 			pointIndex = 0;
@@ -99,9 +123,9 @@ VTK_THREAD_RETURN_TYPE ThreadedImageSlice (void *arg) {
 			Dimensions[0] = orientationDims[0];
 			Dimensions[1] = orientationDims[2];
 			Dimensions[2] = 1;
-			params[0][0] = 1;	params[0][1] = 0;	params[0][2] = 0;
-			params[1][0] = 0;	params[1][1] = 0;	params[1][2] = -1;
-			params[2][0] = 0;	params[2][1] = -1;	params[2][2] = 0;
+			params[0][0]=1;		params[0][1]=0;		params[0][2]=0;
+			params[1][0]=0;		params[1][1]=0;		params[1][2]=-1;
+			params[2][0]=0;		params[2][1]=-1;	params[2][2]=0;
 			Point[0] = 0;
 			Point[2] = 0;
 			pointIndex = 1;
@@ -112,9 +136,9 @@ VTK_THREAD_RETURN_TYPE ThreadedImageSlice (void *arg) {
 			Image->GetDimensions(Dimensions);
 			NumberOfSlices = Dimensions[2];
 			Dimensions[2] = 1;
-			params[0][0] = 1;	params[0][1] = 0;	params[0][2] = 0;
-			params[1][0] = 0;	params[1][1] = -1;	params[1][2] = 0;
-			params[2][0] = 0;	params[2][1] = 0;	params[2][2] = 1;
+			params[0][0]=1;		params[0][1]=0;		params[0][2]=0;
+			params[1][0]=0;		params[1][1]=-1;	params[1][2]=0;
+			params[2][0]=0;		params[2][1]=0;		params[2][2]=1;
 			Point[0] = 0;
 			Point[1] = 0;
 			pointIndex = 2;
@@ -128,13 +152,13 @@ VTK_THREAD_RETURN_TYPE ThreadedImageSlice (void *arg) {
 
 	int NumComponents;
 	switch (Image->GetScalarType()) {
-	case 2: //VTK_CHAR
-	case 15: // VTK_SIGNED_CHAR
-	case 3: // VTK_UNSIGNED_CHAR
+	case 2:
+	case 15:
+	case 3:
 		NumComponents = 1;
 		break;
-	case 4: // VTK_SHORT
-	case 5: // VTK_UNSIGNED_SHORT
+	case 4:
+	case 5:
 		NumComponents = 2;
 		break;
 	default:
@@ -142,55 +166,53 @@ VTK_THREAD_RETURN_TYPE ThreadedImageSlice (void *arg) {
 		break;
 	}
 
-	if (Helper->Format == 0) {
-		Cast->SetOutputScalarTypeToFloat();
+	if ( ( Helper->Format == 0 ) && ( Image->GetNumberOfScalarComponents() == 1 ) ) {
+		Cast->SetOutputScalarTypeToFloat ();
 		Writer = vtkPNGWriter::New();
 		Suffix << ".png";
 		PNGSlice = vtkImageData::New();
 		PNGSlice->SetDimensions(Dimensions);
-		PNGSlice->AllocateScalars(VTK_UNSIGNED_CHAR, NumComponents);
+		PNGSlice->AllocateScalars(VTK_UNSIGNED_CHAR, NumComponents);		
 	} else {
 		Cast->SetOutputScalarTypeToUnsignedChar ();
-		Writer = vtkJPEGWriter::New();
+		Writer = (vtkImageWriter *) vtkJPEGWriter::New();
 		Suffix << ".jpg";
 		if (!Helper->userdefinedscale) {
-			Cast->SetShift( - Helper->Range[0]);
+			Cast->SetShift(-Helper->Range[0]);
 			Cast->SetScale(255 / (Helper->Range[1] - Helper->Range[0]));
 		} else {
 			Cast->SetShift(Helper->Shift);
 			Cast->SetScale(Helper->Scale);
 		}
 	}
-	
-	Slicer->SetInputData(Image);
-	Slicer->SetOutputDimensionality(2);
 
-	for (int i = MyId; i < NumberOfSlices; i += NumberOfThreads) {
+	for (int i = MyId; i < NumberOfSlices;i += NumberOfThreads) {
 		Point[pointIndex] = i;
 		double Coords[3];
 		Image->GetPoint (Image->ComputePointId(Point), Coords);
 		Slicer->SetResliceAxesOrigin(Coords);
 		Slicer->Update();
 		if (Image->GetNumberOfScalarComponents() > 1) {
-			Writer->SetInputData(Slicer->GetOutput());
+			Cast->SetInputData(Slicer->GetOutput());
+			Cast->Update();
+			Writer->SetInputData(Cast->GetOutput());
 		} else {
 			if (Helper->Format == 0) {
 				vtkImageData *Output;
 				unsigned char* SlicePointer;
-				int j;
 				int numPixels;
 
-
 				switch (Image->GetScalarType()) {
-				case 2: //VTK_CHAR
-				case 15: // VTK_SIGNED_CHAR
-				case 3: // VTK_UNSIGNED_CHAR
-				case 4: // VTK_SHORT
-				case 5: // VTK_UNSIGNED_SHORT
+				case 2:
+				case 15:
+				case 3:
+				case 4:
+				case 5:
 					Output = Slicer->GetOutput();
+					Slicer->Update();
 					break;
 				default:
-					Cast->SetInputConnection(Slicer->GetOutputPort());
+					Cast->SetInputData(Slicer->GetOutput());
 					Cast->Update();
 					Output = Cast->GetOutput();
 					break;
@@ -199,13 +221,13 @@ VTK_THREAD_RETURN_TYPE ThreadedImageSlice (void *arg) {
 				unsigned char* PNGPointer = (unsigned char *) PNGSlice->GetScalarPointer();
 				SlicePointer = (unsigned char *) Output->GetScalarPointer();
 				numPixels = Dimensions[0] * Dimensions[1] * NumComponents;
-				for (j = 0; j != numPixels; j++) {
+				for (int j = 0; j != numPixels; j++) {
 					PNGPointer[j] = SlicePointer[j];
 				}
 
 				Writer->SetInputData(PNGSlice);
 			} else {
-				Cast->SetInputConnection(Slicer->GetOutputPort());
+				Cast->SetInputData(Slicer->GetOutput());
 				Cast->Update();
 				Writer->SetInputData(Cast->GetOutput());
 			}
@@ -220,35 +242,35 @@ VTK_THREAD_RETURN_TYPE ThreadedImageSlice (void *arg) {
 	if (Helper->Format == 0) {
 		PNGSlice->Delete();
 	}
-
 	return (VTK_THREAD_RETURN_VALUE);
 }
 
-int main( int argc, char *argv[]) {
+int main( int argc, char *argv[] )
+{
 	MyThreaderHelperClass Helper;
 
 	if (argc<2) {
-		cout << "Usage : VolumeSlice file.mhd [options]"<<endl;
-		cout << "Options : "<<endl;
-		cout << "-f value  : set format (0: png ; 1 : jpg) default : 1"<<endl;
-		cout << "-sc value : set scale "<<endl;
-		cout << "-sh value : set shift "<<endl;
-		cout << "-o value : set output directory"<<endl;
-		cout << "-mhdcolors : create png image without using scale nor shifting"<<endl;
-		cout << "-orientation : set the axe used for the orientation of the slice (0 : z; 1 : x; 2 : y) default : 0"<<endl;
+		cout<<"Usage : VolumeSlice file.mhd [options]"<<endl;
+		cout<<"Options : "<<endl;
+		cout<<"-f value  : set format (0: png ; 1 : jpg) default : 1"<<endl;
+		cout<<"-sc value : set scale "<<endl;
+		cout<<"-sh value : set shift "<<endl;
+		cout<<"-o value : set output directory"<<endl;
+		cout<<"-mhdcolors : create png image without using scale nor shifting"<<endl;
+		cout<<"-orientation : set the axe used for the orientation of the slice (0 : z; 1 : x; 2 : y) default : 0"<<endl;
 		exit(1);
 	}
 	
 	// Load Volume
 	cout << "load : " << argv[1] << endl;
-	
-	vtkMetaImageReader *Reader = vtkMetaImageReader::New();
+	vtkRobustImageReader *Reader = vtkRobustImageReader::New();
 	Reader->SetFileName(argv[1]);
 	Reader->Update();
 	vtkImageData *Image = Reader->GetOutput();
 	Image->GetScalarRange(Helper.Range);
+	cout << "Range : " << Helper.Range[0] << "   " << Helper.Range[1] << endl;
 	
-	Helper.Image = Image;
+	Helper.Image=Image;
 	
 	bool matrixFound = false;
 	bool anOrientation = false;
@@ -256,14 +278,16 @@ int main( int argc, char *argv[]) {
 	// Keep transformation matrix and anatomical orientation of input meta image, it needs to be copied to output images.
 	// Since VTK currently offers no methods to read and write this information, we're forced to use IO streams
 	std::string imageReaderType = Reader->GetClassName();
-	for(int i=0; i<3; ++i)
-		for(int j=0; j<3; ++j)
+	for(int i = 0; i < 3; ++i) {
+		for(int j = 0; j < 3; ++j) {
 			Helper.nativeTransformMtrx[j][i] = 0;
+		}
+	}
 	Helper.nativeTransformMtrx[0][0] = 1;
 	Helper.nativeTransformMtrx[1][1] = 1;
 	Helper.nativeTransformMtrx[2][2] = 1;
-	if(imageReaderType=="vtkMetaImageReader") {
-		ifstream is(argv[1]);
+	if(imageReaderType == "vtkMetaImageReader") {
+		std::ifstream is(argv[1]);
 		std::string line, prefix;
 		while(is) {
 			getline(is, line);
@@ -272,16 +296,15 @@ int main( int argc, char *argv[]) {
 			if((prefix == "TransformMatrix") ||
 				(prefix == "Orientation") ||
 				(prefix == "Rotation")) {
-
 				matrixPrefix = prefix;
 				iss >> prefix; // eat the "=" sign
-				for(int i = 0; i < 3; ++i)
-					for(int j = 0; j < 3; ++j)
+				for(int i = 0; i < 3; ++i) {
+					for(int j = 0; j < 3; ++j) {
 						iss >> Helper.nativeTransformMtrx[j][i];
+					}
+				}
 				matrixFound = true;
-
 			}
-
 			if(prefix == "AnatomicalOrientation") {
 				iss >> prefix; // eat the "=" sign
 				iss >> Helper.anatomicalOrientation;
@@ -295,10 +318,10 @@ int main( int argc, char *argv[]) {
 	int ArgumentsIndex = 2;
 	while (ArgumentsIndex < argc) {
 		if (strcmp(argv[ArgumentsIndex], "-sc") == 0) {
-			double Scale = atof(argv[ArgumentsIndex + 1]);
+			double Scale = atof(argv[ArgumentsIndex+1]);
 			cout << "Scale=" << Scale << endl;
 			Helper.Scale = Scale;
-			Helper.userdefinedscale = true;
+			Helper.userdefinedscale=true;
 		}
 
 		if (strcmp(argv[ArgumentsIndex], "-sh") == 0) {
@@ -309,37 +332,73 @@ int main( int argc, char *argv[]) {
 		}
 
 		if (strcmp(argv[ArgumentsIndex], "-f") == 0) {
-
-			Helper.Format=atoi(argv[ArgumentsIndex + 1]);
-			cout<<"Format: "<<Helper.Format<<endl;
+			Helper.Format = atoi(argv[ArgumentsIndex + 1]);
+			cout << "Format: " << Helper.Format << endl;
 		}
 
 		if (strcmp(argv[ArgumentsIndex], "-o") == 0) {
 			strcpy (Helper.directory, argv[ArgumentsIndex + 1]);
-			cout<<"Output directory : "<< Helper.directory << endl;
+			cout << "Output directory : " << Helper.directory << endl;
 		}
-
-		if (strcmp(argv[ArgumentsIndex], "-orientation") == 0) {
-			int tempVal = atoi(argv[ArgumentsIndex + 1]);
+		
+		if (strcmp(argv[ArgumentsIndex], "-orientation")==0) {
+			int tempVal = atoi(argv[ArgumentsIndex+1]);
 			if((0 <= tempVal) && (tempVal < 3))
 				Helper.xyzOrientation = tempVal;
 		}
 		
+		if (strcmp(argv[ArgumentsIndex],"-uc") == 0) {
+			int tempVal = atoi(argv[ArgumentsIndex+1]);
+			if(tempVal == 1) {
+				vtkImageShiftScale *cast = vtkImageShiftScale::New();
+				cast->SetInputData(Image);
+				cast->SetOutputScalarTypeToUnsignedChar();
+				double min = Helper.Range[0];
+				double max = Helper.Range[1];
+				cast->SetShift(-min);
+				cast->SetScale(255.0 / (max - min));
+				cast->Update();
+				Image->ShallowCopy(cast->GetOutput());
+				cast->Delete();
+			}
+		}
+
+		if (strcmp(argv[ArgumentsIndex],"-s") == 0) {
+			float ratio = atof(argv[ArgumentsIndex+1]);
+			vtkImageResample *Resampler = vtkImageResample::New();
+			Resampler->SetInputData(Image);
+			Resampler->SetInterpolationModeToNearestNeighbor();
+			for (int i = 0; i < 3; i++) {
+				Resampler->SetAxisMagnificationFactor(i, ratio);
+			}
+			Resampler->Update();
+			Image->ShallowCopy(Resampler->GetOutput());
+		}
 		ArgumentsIndex += 2;
 	}
 
 	int Dimensions[3];
 	Image->GetDimensions(Dimensions);
-	cout << "Dimensions : " << Dimensions[0] << " " << Dimensions[1] <<
-		" " << Dimensions[2] << endl;
+	
+	cout << "Dimensions : " << Dimensions[0] << " " <<
+		Dimensions[1] << " " << Dimensions[2] << endl;
 
 	vtkMultiThreader *Threader = vtkMultiThreader::New();
 	Threader->SetSingleMethod (ThreadedImageSlice, (void *) &Helper);
 	cout << "Using " << Threader->GetNumberOfThreads() << " threads" << endl;
-	Threader->SingleMethodExecute();
+
+	// note : the * 3 multiplicator bellow is an attempt at reducing segfaults...
+	for (int i = 0; i < Threader->GetNumberOfThreads() * 3; i++) {
+		vtkImageData *temp = vtkImageData::New();
+		temp->ShallowCopy(Helper.Image);
+		Helper.Images.push_back(temp);
+		Helper.Slicers.push_back(vtkImageReslice::New());
+		Helper.Casters.push_back(vtkImageShiftScale::New());
+	}
+	Threader->SingleMethodExecute ();
 	Threader->Delete();
 
-	//output xml file containint volume details
+	//output xml file containing volume details
 	vtkXMLDataElement *Root = vtkXMLDataElement::New ();
 	Root->SetName("volume");
 
@@ -363,7 +422,7 @@ int main( int argc, char *argv[]) {
 	XMLRange->SetIntAttribute ("type", 	Image->GetScalarType());
 	XMLRange->SetIntAttribute ("size", 	Image->GetScalarSize());
 	char typeString[1000];
-	strcpy(typeString, Image->GetScalarTypeAsString());
+	strcpy(typeString,Image->GetScalarTypeAsString());
 	XMLRange->SetCharacterData (typeString, strlen(typeString)+1);
 	Root->AddNestedElement(XMLRange);
 
@@ -415,11 +474,11 @@ int main( int argc, char *argv[]) {
 	if(anOrientation) {
 		vtkXMLDataElement *XMLAnOr = vtkXMLDataElement::New ();
 		XMLAnOr->SetName("anamori");
-		XMLAnOr->SetCharacterData (Helper.anatomicalOrientation, strlen(Helper.anatomicalOrientation) + 1);
+		XMLAnOr->SetCharacterData (Helper.anatomicalOrientation, strlen(Helper.anatomicalOrientation)+1);
 		Root->AddNestedElement(XMLAnOr);
 	}
 	
-	vtkXMLDataElement *Slices=vtkXMLDataElement::New ();
+	vtkXMLDataElement *Slices = vtkXMLDataElement::New ();
 	Slices->SetIntAttribute ("orientation", Helper.xyzOrientation);
 	Slices->SetIntAttribute ("offset", Helper.offset);
 	Slices->SetIntAttribute ("timestamp",(int) vtkTimerLog::GetUniversalTime());
@@ -428,6 +487,6 @@ int main( int argc, char *argv[]) {
 	Root->AddNestedElement(Slices);
 
 	std::stringstream FileName;
-	FileName << Helper.directory << "volume.xml";
+	FileName<<Helper.directory<<"volume.xml";
 	vtkXMLUtilities::WriteElementToFile (Root, FileName.str().c_str());
 }
---
+++ VolumeProcessing/vtkRobustImageReader.h
@@ -0,0 +1,150 @@
+#ifndef __vtkRobustImageReader_h
+#define __vtkRobustImageReader_h
+
+#include <sstream>
+
+#include <vtkImageData.h>
+#include <vtkImageFlip.h>
+#include <vtkImageReader2Factory.h>
+#include <vtkMatrix4x4.h>
+#include <vtkMetaImageReader.h>
+#include <vtkNIFTIImageReader.h>
+#include <vtkObjectFactory.h>
+
+// v1
+
+class vtkRobustImageReader : public vtkObject 
+{
+
+public :
+
+	static vtkRobustImageReader *New() {
+		vtkObject* ret = vtkObjectFactory::CreateInstance("vtkRobustImageReader");
+		if(ret) {
+			return (vtkRobustImageReader*)ret;
+		}
+
+		return (new vtkRobustImageReader);
+	}
+
+	vtkTypeMacro(vtkRobustImageReader,vtkObject);
+
+	void Update() {
+		vtkImageReader2Factory *imageReaderFactory = vtkImageReader2Factory::New();
+		vtkMetaImageReader *metaImageReader = vtkMetaImageReader::New();
+		imageReaderFactory->RegisterReader(metaImageReader);
+		vtkNIFTIImageReader *niftiiImageReader = vtkNIFTIImageReader::New();
+		imageReaderFactory->RegisterReader(niftiiImageReader);
+
+		// Create a reader for image and try to load it
+		vtkImageReader2* Reader = imageReaderFactory->CreateImageReader2(FileName) ;
+
+		if (!Reader) {
+			std::cerr << "Cannot load file " << FileName << " as an image file; terminating.\n" ;
+			exit(5) ;
+		}
+
+		Reader->SetFileName(FileName);
+		Reader->Update();
+		Output = Reader->GetOutput();
+
+		bool flip[3] = {false, false, false};
+		double Origin[3];
+		double Spacing[3];
+		int Dimensions[3];
+
+		if (strcmp (Reader->GetClassName(), "vtkMetaImageReader") == 0) {
+			bool matrixFound = false;
+			bool anOrientation = false;
+			std::string matrixPrefix;
+			std::string anatomicalOrientation;
+
+			ifstream is(FileName);
+			std::string line, prefix;
+			double transformationMatrix[9] ;
+
+			while(is) {
+				getline(is, line);
+				std::istringstream iss(line);
+				iss >> prefix;
+				if ((prefix=="TransformMatrix") || (prefix == "Orientation") || (prefix == "Rotation")) {
+					matrixPrefix = prefix;
+					iss >> prefix; // eat the "=" sign
+					for (int i = 0; i < 9; i++) {
+						iss >> transformationMatrix[i];
+					}
+
+					for (int i = 0; i < 3; i++) {
+						if (transformationMatrix[4 * i] < 0) {
+							flip[i] = true;
+						}
+					}
+				}
+			}
+		}
+
+		if (strcmp (Reader->GetClassName(), "vtkNIFTIImageReader") == 0) {
+		    vtkNIFTIImageReader *niftiReader = (vtkNIFTIImageReader *) Reader;
+
+            vtkMatrix4x4 *qForm = niftiReader->GetQFormMatrix();
+			if (!qForm) {
+                qForm = vtkMatrix4x4::New();
+            }
+
+            for (int i = 0; i < 3; i++) {
+				if (qForm->GetElement(i, i) < 0) {
+					flip[i] = true;
+				}
+				Origin[i] = qForm->GetElement(i,3);
+			}
+
+
+            if (niftiReader->GetQFac() < 0) {
+				for (int i = 0; i < 2; i++) {
+					flip[i] = !flip[i];
+					Origin[i] = -Origin[i];
+				}
+            }
+
+			Output->SetOrigin(Origin);
+		}
+
+		if (!flip[0] && !flip[1] && !flip[2]) {
+			return;
+		}
+
+		Output->GetSpacing(Spacing);
+		Output->GetDimensions(Dimensions);
+
+        for (int i = 0; i < 3; i++) {
+            if (!flip[i]) continue;
+            std::cout << "Warning : RobustReader flipping dimension " << i << std::endl;
+            vtkImageFlip *flip = vtkImageFlip::New();
+            flip->SetInputData(Output);
+            flip->SetFilteredAxis (i);
+            flip->Update();
+            Output = flip->GetOutput();
+			Output->GetOrigin(Origin);
+            Origin[i] = Origin[i] - Spacing[i] * ( Dimensions[i] - 1);
+			Output->SetOrigin(Origin);
+        }
+	}
+
+	vtkGetObjectMacro(Output, vtkImageData)
+
+	vtkGetMacro(FileName, char*)
+	vtkSetMacro(FileName, char*)
+
+protected :
+	vtkImageData *Output;
+
+	char *FileName;
+
+	vtkRobustImageReader() {
+		Output = 0;
+		FileName = 0;
+	};
+};
+
+#endif
+
