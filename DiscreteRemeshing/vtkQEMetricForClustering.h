eb0ad4b9f85166c2eb30ac1162d44eb28b1cc15e
Sebastien Valette <sebastien.valette@creatis.insa-lyon.fr>
2019-11-18 16:41:01 +0100

    QEM Metric cleanup & prepare

--- DiscreteRemeshing/vtkQEMetricForClustering.h
+++ DiscreteRemeshing/vtkQEMetricForClustering.h
@@ -40,336 +40,321 @@ Auteur:    Sebastien VALETTE
 #include <vtkTriangle.h>
 #include <vtkMath.h>
 #include <vtkDataArrayCollection.h>
-#include "vtkSurface.h"
 
 #include "vtkQuadricTools.h"
 
-class  vtkQEMetricForClustering 
-{
+class  vtkQEMetricForClustering {
+
 public:
 
-	void MultiplyItemWeight(vtkIdType ItemId, double Factor)
-	{
-		for (int i=0;i<3;i++)
-			this->Items[ItemId].Value[i]*=Factor;
-		this->Items[ItemId].Weight*=Factor;
+	void SetQuadricsOptimizationLevel( int L ) {
+
+		this->QuadricsOptimizationLevel = L;
+
 	}
 
-	void SetQuadricsOptimizationLevel(int L)
-	{
-		this->QuadricsOptimizationLevel=L;
+	void SetConstrainedClustering( int C ) {
+
+		cout << "Setting Metric Constraint to "<< C << endl;
+		this->ActiveConstraintsFlag = C;
+
 	}
 
-	void SetConstrainedClustering(int C)
-	{
-		cout<<"Setting Metric Constraint to "<<C<<endl;
-		this->ActiveConstraintsFlag=C;
-	}
+	int IsCurvatureIndicatorNeeded() {
 
-	int IsCurvatureIndicatorNeeded()
-	{
-		if (this->Gradation>0)
-			return (1);
-		else
-			return (0);
+		return this->Gradation > 0 ? 1 : 0;
+
 	}
 	
-	int IsPrincipalDirectionsNeeded()
-	{
-		return (0);
+	int IsPrincipalDirectionsNeeded() {
+
+		return 0;
+
 	}
 	
-	void SetCurvatureInfo(vtkDataArrayCollection *Info)
-	{
-		if (this->CustomWeights)
+	void SetCurvatureInfo( vtkDataArrayCollection *Info ) {
+
+		if ( this->CustomWeights )
 			this->CustomWeights->Delete();
-		this->CustomWeights=(vtkDoubleArray*) Info->GetItem(0);
-		this->CustomWeights->Register(this->Object);		
+
+		this->CustomWeights=( vtkDoubleArray* ) Info->GetItem( 0 );
+		this->CustomWeights->Register( this->Object );
+
 	}
 
-	void SetGradation(double Gradation)
-	{
-		this->Gradation=Gradation;
+	void SetGradation(double Gradation) {
+
+		this->Gradation = Gradation;
+
 	}
 
-	double GetGradation()
-	{
-		return (this->Gradation);
+	double GetGradation() {
+
+		return this->Gradation;
+
 	}
 
 	// each item is represented by a quadric matrix
-	struct Item
-	{
-		// the 4x4 quadric in compact form, without the 10th useless coefficient
-		double Quadric[9];
+	struct Item {
 
-		// the barycenter (multiplied by its weight)
-		double Value[3];
+		// 4x4 quadric in compact form, without the 10th useless coefficient
+		double Quadric[ 9 ];
 
-		// the weight
+		// barycenter (multiplied by its weight)
+		double Value[ 3 ];
+
+		// weight
 		double Weight;
 	};
 
-	struct Cluster 
-	{
+	struct Cluster {
+
 		// Value of the energy term for this cluster (cached to increase speed by about 33%)
 		double EnergyValue;
 
-		// The accumulated quadrics, without the 10th useless coefficient
-		double SQuadric[9];
+		// accumulated quadrics, without the 10th useless coefficient
+		double SQuadric[ 9 ];
 
-		// The cluster best approximating point (based on quadrics)
-		double Centroid[3];
+		// cluster best approximating point ( centroid, quadric-based or fixed vertex)
+		double Centroid[ 3 ];
 
-		// The accumulated centroids (weighted)
-		double SValue[3];
+		// accumulated centroids (weighted)
+		double SValue[ 3 ];
 
-		// the accumulated weights
+		// accumulated weights
 		double SWeight;
 
 		/// gives the number of quadric eigenvalues equal to zero (could be 0, 1 or 2)
 		char RankDeficiency;
+
+		vtkIdType AnchorVertex;
 	};
 
-	int GetClusterRankDeficiency(Cluster *C)
-	{
+	int GetClusterRankDeficiency( Cluster *C ) {
+
 		return C->RankDeficiency;
-	}
-	void ResetItem(Item *I) 
-	{
-		int i;
-		for (i=0;i<9;i++)
-			I->Quadric[i]=0;
-		for (i=0;i<3;i++)
-			I->Value[i]=0;
-		I->Weight=0;
+
 	}
 
-	double GetItemWeight(vtkIdType ItemId) 
-	{
-		return this->Items[ItemId].Weight;
+	void ResetItem( Item *I ) {
+
+		for ( int i = 0; i < 9; i++ ) I->Quadric[ i ] = 0;
+		for ( int i = 0; i < 3; i++ ) I->Value[ i ] = 0;
+		I->Weight = 0;
+
 	}
 
-	void ComputeVertexQuadric(Item *I,vtkSurface *Mesh,int Vertex,double Weight)
-	{
-		this->ResetItem(I);
-		int i;
+	double GetItemWeight( vtkIdType ItemId ) {
+
+		return this->Items[ ItemId ].Weight;
+
+	}
+
+	void ComputeVertexQuadric( Item *I, vtkSurface *Mesh, int Vertex, double Weight ) {
+
+		this->ResetItem( I );
 		vtkIdList *FList=vtkIdList::New();
 		Mesh->GetVertexNeighbourFaces(Vertex,FList);
 
-		for (i=0;i<FList->GetNumberOfIds();i++)
-			vtkQuadricTools::AddTriangleQuadric(I->Quadric,Mesh,FList->GetId(i),false);                
+		for ( int i = 0; i < FList->GetNumberOfIds(); i++ )
+			vtkQuadricTools::AddTriangleQuadric( I->Quadric, Mesh, FList->GetId( i ), false );
 
-		Mesh->GetPoint(Vertex,I->Value);
-		for (i=0;i<3;i++)
-			I->Value[i]=I->Value[i]*Weight;
-		I->Weight=Weight;
+		Mesh->GetPoint( Vertex, I->Value );
+		for ( int i = 0; i < 3; i++) I->Value[ i ] = I->Value[ i ] * Weight;
+		I->Weight = Weight;
 		FList->Delete();
+
 	}
 
-	void ComputeTriangleQuadric(Item *I,vtkSurface *Mesh,int Face,double Weight)
-	{
+	void ComputeTriangleQuadric( Item *I,vtkSurface *Mesh, int Face, double Weight ) {
+
+		double x1[ 3 ], x2[ 3 ], x3[ 3 ];
+		vtkIdType V1, V2, V3;
 		this->ResetItem(I);
-		vtkQuadricTools::AddTriangleQuadric(I->Quadric,Mesh,Face,false);
+		vtkQuadricTools::AddTriangleQuadric( I->Quadric, Mesh, Face, false);
+		Mesh->GetFaceVertices( Face, V1, V2, V3 );
+		Mesh->GetPointCoordinates( V1, x1 );
+		Mesh->GetPointCoordinates( V2, x2 );
+		Mesh->GetPointCoordinates( V3, x3 );
+		I->Weight = Weight;
 
-		double x1[3],x2[3],x3[3];
-		vtkIdType V1,V2,V3;
-		Mesh->GetFaceVertices(Face,V1,V2,V3);
-		Mesh->GetPointCoordinates(V1,x1);
-		Mesh->GetPointCoordinates(V2,x2);
-		Mesh->GetPointCoordinates(V3,x3);
-		int i;
-		for (i=0;i<3;i++)
-		{
-			I->Value[i]=Weight*(x1[i]+x2[i]+x3[i])/3.0;
-		}
-		I->Weight=Weight;
+		for ( int i = 0; i < 3; i++ )
+			I->Value[ i ] = Weight * ( x1[ i ] + x2[ i ] + x3[ i ] ) / 3.0;
+
 	}
 
-	double GetClusterEnergy(Cluster *C)
-	{
+	double GetClusterEnergy( Cluster *C ) {
+
 		return C->EnergyValue;
+
 	}
 
-	void ComputeClusterEnergy(Cluster *C)
-	{
+	void ComputeClusterEnergy(Cluster *C) {
+
 		C->EnergyValue= (
-		C->Centroid[0]*C->Centroid[0]
-		+C->Centroid[1]*C->Centroid[1]
-		+C->Centroid[2]*C->Centroid[2])*C->SWeight
-		-2.0*vtkMath::Dot(C->Centroid,C->SValue);			
+			C->Centroid[ 0 ] * C->Centroid[ 0 ]
+			+ C->Centroid[ 1 ]*C->Centroid[ 1 ]
+			+ C->Centroid[ 2 ]*C->Centroid[ 2 ]) * C->SWeight
+			-2.0 * vtkMath::Dot( C->Centroid, C->SValue );
 	}
 
-	void DeepCopy(Cluster *Source,Cluster *Destination)
-	{
-		int i;
-		Destination->SWeight=Source->SWeight;
-		for (i=0;i<3;i++)
-		{
-			Destination->SValue[i]=Source->SValue[i];
-			Destination->Centroid[i]=Source->Centroid[i];
+	void DeepCopy( Cluster *Source, Cluster *Destination ) {
+
+		Destination->SWeight = Source->SWeight;
+
+		for ( int i = 0; i < 3; i++ ) {
+
+			Destination->SValue[ i ] = Source->SValue[ i ];
+			Destination->Centroid[ i ] = Source->Centroid[ i ];
+
 		}
-		for (i=0;i<9;i++)
-			Destination->SQuadric[i]=Source->SQuadric[i];
-		Destination->EnergyValue=Source->EnergyValue;
-		Destination->RankDeficiency=Source->RankDeficiency;
+
+		for ( int i = 0; i < 9; i++ )
+			Destination->SQuadric[ i ] = Source->SQuadric[ i ];
+
+		Destination->EnergyValue = Source->EnergyValue;
+		Destination->RankDeficiency = Source->RankDeficiency;
+
 	}
 
-	void Add(Cluster *Source, vtkIdType ItemId, Cluster *Destination)
-	{
-		this->DeepCopy(Source,Destination);
-		this->AddItemToCluster(ItemId, Destination);
+	void Add( Cluster *Source, vtkIdType ItemId, Cluster *Destination ) {
+
+		this->DeepCopy( Source, Destination );
+		this->AddItemToCluster( ItemId, Destination );
+
 	}
 
-	void AddItemToCluster(vtkIdType ItemId, Cluster *C)
-	{
-		Item *I=&this->Items[ItemId];
-		int i;
-		for (i=0;i<3;i++)
-		{
-			C->SValue[i]+=I->Value[i];
-		}
-		for (i=0;i<9;i++)
-			C->SQuadric[i]+=I->Quadric[i];
+	void AddItemToCluster( vtkIdType ItemId, Cluster *C ) {
+
+		Item *I = &this->Items[ ItemId ];
+		for ( int i = 0; i < 3; i++ ) C->SValue[ i ] += I->Value[ i ];
+		for ( int i = 0; i < 9; i++) C->SQuadric[ i ] += I->Quadric[ i ];
 		C->SWeight+=I->Weight;
 	}
 
-	void Sub(Cluster *Source, vtkIdType ItemId, Cluster *Destination)
-	{
-		this->DeepCopy(Source,Destination);
-		this->SubstractItemFromCluster(ItemId, Destination);		
+	void Sub( Cluster *Source, vtkIdType ItemId, Cluster *Destination ) {
+
+		this->DeepCopy( Source,Destination );
+		this->SubstractItemFromCluster( ItemId, Destination );
+
 	}
 
-	void SubstractItemFromCluster(vtkIdType ItemId, Cluster *C)
-	{
-		Item *I=&this->Items[ItemId];
-		int i;
-		for (i=0;i<3;i++)
-		{
-			C->SValue[i]-=I->Value[i];
-		}
+	void SubstractItemFromCluster( vtkIdType ItemId, Cluster *C) {
 
-		for (i=0;i<9;i++)
-			C->SQuadric[i]-=I->Quadric[i];
-		C->SWeight-=I->Weight;
+		Item *I = &this->Items[ ItemId ];
+		for ( int i = 0; i < 3; i++ ) C->SValue[ i ] -= I->Value[ i ];
+		for ( int i = 0; i < 9; i++ ) C->SQuadric[ i ] -= I->Quadric[ i ];
+		C->SWeight -= I->Weight;
+
 	}
 
-	void GetClusterCentroid(Cluster *C,double *P)
-	{
-		P[0]=C->Centroid[0];
-		P[1]=C->Centroid[1];
-		P[2]=C->Centroid[2];
+	void GetClusterCentroid( Cluster *C, double *P ) {
+
+		for ( int i = 0; i < 3; i++ ) P[ i ]=C->Centroid[ i ];
 	}
 
-	void ComputeClusterCentroid(Cluster *C)
-	{
-		C->Centroid[0]=C->SValue[0]/C->SWeight;
-		C->Centroid[1]=C->SValue[1]/C->SWeight;
-		C->Centroid[2]=C->SValue[2]/C->SWeight;
+	void ComputeClusterCentroid( Cluster *C ) {
+
+		for ( int i = 0; i < 3; i++) C->Centroid[ i ] = C->SValue[ i ] / C->SWeight;
 		
-		if (this->ActiveConstraintsFlag==0)
+		if ( !this->ActiveConstraintsFlag || !this->QuadricsOptimizationLevel )
 			return;
 
-		C->RankDeficiency=vtkQuadricTools::ComputeRepresentativePoint(
-					C->SQuadric,C->Centroid,this->QuadricsOptimizationLevel);
+		C->RankDeficiency = vtkQuadricTools::ComputeRepresentativePoint(
+			C->SQuadric, C->Centroid, this->QuadricsOptimizationLevel );
+
 	}
 
-	void ResetCluster(Cluster *C) 
-	{
-		int i;
-		for (i=0;i<9;i++)
-			C->SQuadric[i]=0;
-		for (i=0;i<3;i++)
-		{
-			C->SValue[i]=0;
-			C->Centroid[i]=0;
-		}
-		C->SWeight=0;
-		C->EnergyValue=0;
+	void ResetCluster( Cluster *C ) {
+
+		for ( int i = 0; i < 9; i++ ) C->SQuadric[ i ] = 0;
+		for ( int i = 0; i < 3; i++ ) C->SValue[ i ]=0;
+		for ( int i = 0; i < 3; i++ ) C->Centroid[ i ]=0;
+		C->SWeight = 0;
+		C->EnergyValue = 0;
+
 	}
 
-	void BuildMetric( vtkSurface *Mesh, int ClusteringType )
-	{
-		vtkIdType i;
+	void BuildMetric( vtkSurface *Mesh, int ClusteringType ) {
 
-		if (ClusteringType==0)
-		{
+		if ( ClusteringType == 0 ) {
 			// Items are triangles
 			vtkIdType v1,v2,v3;
-			double P1[3],P2[3],P3[3];
-			Items=new Item[Mesh->GetNumberOfCells()];
-			for (i=0;i<Mesh->GetNumberOfCells();i++)
-			{
-				Mesh->GetFaceVertices(i,v1,v2,v3);
-				Mesh->GetPoint(v1,P1);
-				Mesh->GetPoint(v2,P2);
-				Mesh->GetPoint(v3,P3);
-				if (Gradation==0)
-					Items[i].Weight=vtkTriangle::TriangleArea(P1,P2,P3);
-				else
-					Items[i].Weight=vtkTriangle::TriangleArea(P1,P2,P3)*pow(CustomWeights->GetValue(i),Gradation);
+			double P1[ 3 ],P2[ 3 ],P3[ 3 ];
+			Items = new Item[ Mesh->GetNumberOfCells() ];
+
+			for ( vtkIdType i=0;i < Mesh->GetNumberOfCells(); i++ ) {
+
+				Mesh->GetFaceVertices( i, v1, v2, v3 );
+				Mesh->GetPoint( v1, P1 );
+				Mesh->GetPoint( v2, P2 );
+				Mesh->GetPoint( v3, P3 );
+
+				Items[ i ].Weight = vtkTriangle::TriangleArea( P1, P2, P3 );
+				if ( Gradation > 0 ) Items[ i ].Weight *=
+					pow( CustomWeights->GetValue( i ), Gradation );
 
 			}
-			this->ClampWeights(Items,Mesh->GetNumberOfCells(),10000);
-			for (i=0;i<Mesh->GetNumberOfCells();i++)
-			{
-				this->ComputeTriangleQuadric(Items+i,Mesh,i,Items[i].Weight);
-			}
 
-		}
-		else
-		{
+			this->ClampWeights( Items, Mesh->GetNumberOfCells(), 10000 );
+			for ( vtkIdType i = 0; i < Mesh->GetNumberOfCells(); i++ )
+				this->ComputeTriangleQuadric( Items + i, Mesh, i, Items[ i ].Weight );
+
+		} else {
+
 			// Items are vertices
-			Items=new Item[Mesh->GetNumberOfPoints()];
-			vtkDoubleArray *VerticesAreas=Mesh->GetVerticesAreas();
-			for (i=0;i<Mesh->GetNumberOfPoints();i++)
-			{
-				if (Gradation==0)
-					Items[i].Weight=VerticesAreas->GetValue(i);
-				else
-					Items[i].Weight=VerticesAreas->GetValue(i)*pow(CustomWeights->GetValue(i),Gradation);
+			Items = new Item[ Mesh->GetNumberOfPoints() ];
+			vtkDoubleArray *VerticesAreas = Mesh->GetVerticesAreas();
+			for ( vtkIdType i = 0; i < Mesh->GetNumberOfPoints(); i++ ) {
+
+				Items[i].Weight = VerticesAreas->GetValue(i);
+
+				if ( Gradation > 0 ) Items[i].Weight *=
+					pow(CustomWeights->GetValue( i ), Gradation );
+
 			}
-			this->ClampWeights(Items,Mesh->GetNumberOfPoints(),10000);
-			for (i=0;i<Mesh->GetNumberOfPoints();i++)
-			{
-				this->ComputeVertexQuadric(Items+i,Mesh,i,Items[i].Weight);
-			}
+
+			this->ClampWeights( Items,Mesh->GetNumberOfPoints(), 10000 );
+			for ( vtkIdType i = 0; i < Mesh->GetNumberOfPoints();i++ )
+				this->ComputeVertexQuadric( Items + i, Mesh, i, Items[ i ].Weight );
+
 		}
-	}
-	// this method clamps the weights between AverageValue/Ratio and AverageValue*Ratio
-	void ClampWeights(Item *Items,int NumberOfValues,double Ratio)
-	{
-		double Average=0;
-		int i;
-		for (i=0;i<NumberOfValues;i++)
-		{
-			Average+=Items[i].Weight;
-		}
-		Average=Average/(double)NumberOfValues;
-		double Min=Average/Ratio;
-		double Max=Average*Ratio;
-		for (i=0;i<NumberOfValues;i++)
-		{
-			if (Items[i].Weight>Max)
-				Items[i].Weight=Max;
-			if (Items[i].Weight<Min)
-				Items[i].Weight=Min;
-		}
+
 	}
 
-	vtkQEMetricForClustering()
-	{
-		this->CustomWeights=0;
-		this->Gradation=0;
-		this->ActiveConstraintsFlag=1;
-		this->Object=vtkObject::New();	
-		this->QuadricsOptimizationLevel=3;	
+	// this method clamps the weights between AverageValue/Ratio and AverageValue*Ratio
+	void ClampWeights( Item *Items, int NumberOfValues, double Ratio ) {
+
+		double Average = 0;
+		for ( int i = 0; i < NumberOfValues; i++ ) Average += Items[ i ].Weight;
+		Average /= (double) NumberOfValues;
+		double Min = Average/Ratio;
+		double Max = Average*Ratio;
+
+		for ( int i = 0; i < NumberOfValues; i++ ) {
+
+			if ( Items[ i ].Weight > Max ) Items[ i ].Weight = Max;
+			if ( Items[ i ].Weight < Min ) Items[ i ].Weight = Min;
+
+		}
+
+	}
+
+	vtkQEMetricForClustering() {
+
+		this->CustomWeights = 0;
+		this->Gradation = 0;
+		this->ActiveConstraintsFlag = 1;
+		this->Object = vtkObject::New();
+		this->QuadricsOptimizationLevel = 3;
+
 	};
-	~vtkQEMetricForClustering()
-	{
+
+	~vtkQEMetricForClustering() {
+
 		this->Object->Delete();
-		if (this->CustomWeights)
-			this->CustomWeights->Delete();
+		if ( this->CustomWeights ) this->CustomWeights->Delete();
+
 	};
 
 private:
@@ -377,13 +362,10 @@ private:
 	int	ActiveConstraintsFlag;
 	vtkDoubleArray *CustomWeights;
 	int QuadricsOptimizationLevel;
-
-	// Dummy object used for registering the curvature indicators
-	vtkObject	*Object;	
+	vtkObject *Object; // Dummy object used for registering the curvature indicators
 	double Gradation;
-	
-	// The array storing items
-	Item *Items;
+	Item *Items; // array storing items
+
 };
 
 #endif
