e583e278f0da060cf970592412e0753645c5ec7c
Sebastien Valette <sebastien.valette@creatis.insa-lyon.fr>
2022-06-27 10:03:41 +0200

    fix test.sh

--- test.sh
+++ test.sh
@@ -6,6 +6,7 @@ bin/ACVDQ fandisk.obj 3000 0
 wget https://github.com/alecjacobson/common-3d-test-models/raw/master/data/horse.obj
 bin/AnisotropicRemeshingQ horse.obj 1000 1.5
 wget http://graphics.stanford.edu/data/3Dscanrep/xyzrgb/xyzrgb_statuette.ply.gz
+gunzip xyzrgb_statuette.ply.gz
 bin/ACVDQ xyzrgb_statuette.ply 100000 1.5
 bin/ACVDQP xyzrgb_statuette.ply 100000 1.5
 bin/ACVDQP xyzrgb_statuette.ply 100000 1.5 -np 3
