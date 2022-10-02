ca394565460d1c45e2989e52a09b2449d6ec27d4
Sebastien Valette <sebastien.valette@creatis.insa-lyon.fr>
2022-10-02 22:31:24 +0200

    improve VolumeCrop and expose it to DESK

--- VolumeProcessing/Examples/VolumeCrop.cxx
+++ VolumeProcessing/Examples/VolumeCrop.cxx
@@ -12,34 +12,33 @@ Auteur:   Sebastien Valette
 
 #include <sstream>
 #include <vtkImageData.h>
-#include <vtkMetaImageWriter.h>
-#include <vtkMetaImageReader.h>
 #include <vtkExtractVOI.h>
+#include <vtkNIFTIImageWriter.h>
+
+#include "vtkRobustImageReader.h"
 
 int main( int argc, char *argv[] )
 {
 
 	if (argc<9)
 	{
-		cout<<"Usage : VolumeCrop in.mhd out.mhd xmin xmax ymin ymax zmin zmax"<<endl;
+		cout<<"Usage : VolumeCrop in.mhd out.nii.gz xmin xmax ymin ymax zmin zmax"<<endl;
 		exit(1);
 	}
 
 	// Load Volume
 	cout <<"load : "<<argv[1]<<endl;
 
-
 	// read image
-	vtkMetaImageReader *Reader=vtkMetaImageReader::New();
+	vtkRobustImageReader *Reader=vtkRobustImageReader::New();
 	Reader->SetFileName(argv[1]);
 	Reader->Update();
 
 	// extract VOI
 	int VOI[6];
 	for (int i=0;i<6;i++)
-	{
 		VOI[i]=atoi(argv[i+3]);
-	}
+
 	vtkExtractVOI *Extract=vtkExtractVOI::New();
 	Extract->SetVOI(VOI);
 	Extract->SetInputData(Reader->GetOutput());
@@ -52,8 +51,17 @@ int main( int argc, char *argv[] )
 	Image->GetDimensions(Dimensions);
 	cout<<"New image dimensions : "<<Dimensions[0]<<" "<<Dimensions[1]<<" "<<Dimensions[2]<<endl;
 
-	vtkMetaImageWriter *Writer=vtkMetaImageWriter::New();
+	double origin[ 3 ], spacing[ 3 ];
+	Reader->GetOutput()->GetOrigin( origin );
+	Reader->GetOutput()->GetSpacing( spacing );
+
+	for ( int i = 0; i < 3; i++ )
+		origin[ i ] = origin[ i ] + spacing[ i ] * VOI[ 2 * i ];
+
+	Image->SetOrigin( origin );
+	vtkNIFTIImageWriter *Writer=vtkNIFTIImageWriter::New();
 	Writer->SetInputData(Image);
 	Writer->SetFileName(argv[2]);
 	Writer->Write();
+
 }
