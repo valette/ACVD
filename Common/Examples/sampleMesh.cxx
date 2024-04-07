f554822f1b5581e3a24a34036f5577556db7d915
Sebastien Valette <sebastien.Valette@creatis.insa-lyon.fr>
2024-04-07 20:42:58 +0200

    better mesh sampling

--- Common/Examples/sampleMesh.cxx
+++ Common/Examples/sampleMesh.cxx
@@ -9,14 +9,14 @@ Auteur:   Sebastien Valette
 // .NAME sampleMesh
 // .SECTION Description
 
+#include <random>
 #include <vtkTimerLog.h>
 #include <vtkMinimalStandardRandomSequence.h>
 #include "vtkSurface.h"
 
-
 int main( int argc, char *argv[] )
 {
-	if (argc < 2) {
+	if (argc < 3) {
 		cout << "Usage : sampleMesh mesh numSamples" << endl;
 		exit(1);
 	}
@@ -31,57 +31,48 @@ int main( int argc, char *argv[] )
 	mesh->DisplayMeshProperties();
 	int numTris = mesh->GetNumberOfCells();
 	double sArea = 0;
+	std::vector<float> sAreas;
+
 	for (int i = 0; i < numTris; i++) {
 		sArea += mesh->GetFaceArea(i);
+		sAreas.push_back( sArea );
 	}
 	std::cout << "Total area : " << sArea << std::endl;
 
-	int numSamples = atoi(argv[2]);
+	int numSamples = atoi( argv[ 2 ] );
 	vtkIdType v1, v2, v3;
-	double P1[3], P2[3], P3[3], N[3];
-	int n2 = 0;
-	double sample[3];
-
-	vtkMinimalStandardRandomSequence *rng = vtkMinimalStandardRandomSequence::New();
-
+	double P1[3], P2[3], P3[3], N[3], sample[3];
+	std::random_device seeder;
+	std::mt19937 generator( seeder() );
+	std::uniform_real_distribution<float> rng( 0.0, sArea );
+	std::uniform_real_distribution<float> rng2(0.0, 1.0);
 	std::ofstream fileOutput;
 	fileOutput.open ("output.xyz");
 
-	for (int i = 0; i < numTris; i++) {
-		double area = mesh->GetFaceArea(i);
-		mesh->GetTriangleNormal(i,N);
-		mesh->GetFaceVertices(i, v1, v2, v3);
+	for (int i = 0; i < numSamples; i++) {
+		auto iter = lower_bound( sAreas.begin(), sAreas.end(), rng( generator ) );
+		int index = iter - sAreas.begin();
+		mesh->GetFaceVertices(index, v1, v2, v3);
+		mesh->GetTriangleNormal(index, N);
 		mesh->GetPointCoordinates(v1, P1);
 		mesh->GetPointCoordinates(v2, P2);
 		mesh->GetPointCoordinates(v3, P3);
-		int n = floor(0.5 + numSamples * area / sArea);
-		n2 +=n;
-		for (int j = 0; j < n ; j++) {
-		    bool ok = false;
-		    double x1, x2, x3;
-		    while (!ok) {
-    			x1 = rng->GetValue();
-    			rng->Next();
-    			x2 = rng->GetValue();
-    			rng->Next();
-    			if (x1 + x2 <= 1.0) {
-    			    ok = true;
-    			}
-		    }
-			x3 = 1 - x1 - x2;
+		float r1 = rng2(generator);
+		float r2 = rng2(generator);
+		float x1 = 1 - sqrt( r1 );
+		float x2 = sqrt( r1 ) * ( 1 - r2 );
+		float x3 = r2 * sqrt( r1 );
 
-			for (int k = 0; k < 3; k++) {
-				sample[k] = P1[k] * x1 + P2[k] * x2 + P3[k] * x3;
-			}
-			fileOutput << sample[0] << '\t' << sample[1] << '\t'
-				<< sample[2] << '\t' << N[0] << '\t' << N[1]
-				<< '\t' << N[2];
-			if ( ( i < numTris - 1 ) || ( j < n - 1 ) ) fileOutput << std::endl;
-		}
+		for (int k = 0; k < 3; k++)
+			sample[k] = P1[k] * x1 + P2[k] * x2 + P3[k] * x3;
+
+		fileOutput << sample[0] << '\t' << sample[1] << '\t'
+			<< sample[2] << '\t' << N[0] << '\t' << N[1]
+			<< '\t' << N[2];
+		if ( i < numSamples - 1 ) fileOutput << std::endl;
+
 	}
-	cout << n2 << " samples thrown" << endl;
 	Timer->StopTimer();
 	fileOutput.close();
-
 	cout << "Done in " << Timer->GetElapsedTime() << "s" << endl;
 }
