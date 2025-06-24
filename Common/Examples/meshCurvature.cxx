3ba2cd9498a607a7a02554293bdb8f9ae9e38b4d
Sebastien Valette <sebastien.Valette@creatis.insa-lyon.fr>
2025-06-24 16:42:01 +0200

    meshCurvature : allow to compute curvature on cells

--- Common/Examples/meshCurvature.cxx
+++ Common/Examples/meshCurvature.cxx
@@ -33,6 +33,8 @@ Author:   Sebastien Valette
 // .NAME meshCurvature
 // .SECTION Description
 
+#include <vtkCellData.h>
+#include <vtkPointData.h>
 #include "vtkSurface.h"
 #include "vtkCurvatureMeasure.h"
 
@@ -40,25 +42,47 @@ int main( int argc, char *argv[] )
 {
 
 	if (argc<2) {
-		cout<<"Usage : meshCurvature inputMesh outputMesh"<<endl;
+		cout<<"Usage : meshCurvature inputMesh [-o outputFileName] [-t elementsType] "<<endl;
 		exit(1);
 	}
 
 	const char *outputFile = "output.vtp";
+	int elementsType = 1; // 1 for points, 0 for cells
+
+	int ArgumentsIndex = 2;
+	while (ArgumentsIndex < argc) {
+		char* key = argv[ArgumentsIndex];
+		char* value = argv[ArgumentsIndex + 1];
+
+		if (strcmp(key, "-o") == 0) {
+			outputFile = argv[ 2 ];
+		}
+
+		if (strcmp(key, "-t") == 0) {
+			elementsType = atoi( value );
+		}
+		ArgumentsIndex += 2;
+	}
+
 	vtkSurface *Mesh = vtkSurface::New();
 	Mesh->CreateFromFile( argv[ 1 ] );
-	if (argc > 2) {
-		outputFile = argv[ 2 ];
-	}
 	cout << "Input mesh: " << argv[ 1 ] << endl;
 
 	vtkCurvatureMeasure *curvatureMeasure = vtkCurvatureMeasure::New();
 	curvatureMeasure->SetInputData( Mesh );
-	curvatureMeasure->SetElementsType( 1 ); // 1 for points, 0 for cells
+	curvatureMeasure->SetElementsType( elementsType ); // 1 for points, 0 for cells
 	curvatureMeasure->SetComputationMethod( 1 ); // 1 for polynomial fitting
 	auto curvatureIndicator = curvatureMeasure->GetCurvatureIndicator()->GetItem( 0 );
-	Mesh->GetPointData()->AddArray( curvatureIndicator );
-	Mesh->GetPointData()->SetActiveScalars( curvatureIndicator->GetName() );
+	if ( elementsType == 0 ) {
+		Mesh->GetCellData()->AddArray( curvatureIndicator );
+		Mesh->GetCellData()->SetActiveScalars( curvatureIndicator->GetName() );
+		cout << "Curvature computed for cells." << endl;
+	} else if ( elementsType == 1 ) {
+		Mesh->GetPointData()->AddArray( curvatureIndicator );
+		Mesh->GetPointData()->SetActiveScalars( curvatureIndicator->GetName() );
+		cout << "Curvature computed for points." << endl;
+	}
+
 	Mesh->WriteToFile( outputFile );
 	cout << "Output mesh: " << outputFile << endl;
 }
