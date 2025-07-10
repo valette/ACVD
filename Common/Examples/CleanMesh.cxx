cea3e67fd0b5c016c6ac140e00ea8b06606be486
Sebastien Valette <sebastien.valette@creatis.insa-lyon.fr>
2025-07-10 12:50:36 +0200

    improve CleanMesh

--- Common/Examples/CleanMesh.cxx
+++ Common/Examples/CleanMesh.cxx
@@ -3,22 +3,22 @@
 * Copyright (c) CREATIS-LRMN (Centre de Recherche en Imagerie Medicale)
 * Author : Sebastien Valette
 *
-*  This software is governed by the CeCILL-B license under French law and 
-*  abiding by the rules of distribution of free software. You can  use, 
-*  modify and/ or redistribute the software under the terms of the CeCILL-B 
-*  license as circulated by CEA, CNRS and INRIA at the following URL 
-*  http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html 
+*  This software is governed by the CeCILL-B license under French law and
+*  abiding by the rules of distribution of free software. You can  use,
+*  modify and/ or redistribute the software under the terms of the CeCILL-B
+*  license as circulated by CEA, CNRS and INRIA at the following URL
+*  http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
 *  or in the file LICENSE.txt.
 *
 *  As a counterpart to the access to the source code and  rights to copy,
 *  modify and redistribute granted by the license, users are provided only
 *  with a limited warranty  and the software's author,  the holder of the
 *  economic rights,  and the successive licensors  have only  limited
-*  liability. 
+*  liability.
 *
 *  The fact that you are presently reading this means that you have had
 *  knowledge of the CeCILL-B license and that you accept its terms.
-* ------------------------------------------------------------------------ */     
+* ------------------------------------------------------------------------ */
 
 #include <vtkCommand.h>
 #include "vtkSurface.h"
@@ -28,9 +28,25 @@ int main( int argc, char *argv[] )
 {
 	if (argc<3)
 	{
-		cout<<"Usage : CleanMesh input output"<<endl;
+		cout<<"Usage : CleanMesh input output [-d 0/1]"<<endl;
 		exit (1);
 	}
+
+
+	int display = 0; // defines whether there will be a graphic display (0: No, 1: yes)
+	int ArgumentsIndex = 3;
+	while (ArgumentsIndex < argc) {
+		char* key = argv[ArgumentsIndex];
+		char* value = argv[ArgumentsIndex + 1];
+
+		if (strcmp(key, "-d") == 0) {
+			display = atoi(value);
+			cout << "Display=" << display << endl;
+		}
+
+		ArgumentsIndex += 2;
+	}
+
 	vtkSurface *Mesh=vtkSurface::New();
 	Mesh->CreateFromFile(argv[1]);
 	vtkPolyData *OriginalMesh=vtkPolyData::New();
@@ -70,7 +86,7 @@ int main( int argc, char *argv[] )
 					if (List->GetNumberOfIds()==1)
 						NumberOfBoundaryEdges++;
 				}
-				if ((NumberOfNonManifoldEdges!=0)&&(NumberOfNonManifoldEdges+NumberOfBoundaryEdges>Rank))
+				if (NumberOfNonManifoldEdges>0)
 				{
 					Mesh->DeleteFace(i);
 					NumberOfCleanups++;
@@ -89,10 +105,14 @@ int main( int argc, char *argv[] )
 		}
 	}
 
+
 	Mesh->CheckNormals();
 	vtkSurface *CleanMesh=Mesh->CleanMemory();
 	cout<<"Output : "<<endl;
 	CleanMesh->DisplayMeshProperties();
+	CleanMesh->WriteToFile(argv[2]);
+
+	if ( display == 0 ) exit( 0 );
 
 	RenderWindow *Window=RenderWindow::New();
 	Window->SetInputData(OriginalMesh);
